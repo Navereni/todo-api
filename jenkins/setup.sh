704e45f6e6cc6baeb24a84075a2804fdb523b543
Ubuntu <krzysztof@dev-jenkins.rtudhuqav13e5lf1sxn3p0wzyb.fx.internal.cloudapp.net>
2021-11-29 12:14:36 +0000

    improved test stage

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -4,11 +4,13 @@ echo "Setup Stage"
 
 # apt dependencies
 sudo apt-get update
-sudo apt-get install -y curl jq
+sudo apt-get install -y curl jq python3-venv
 
 # install docker
-curl https://get.docker.com | sudo bash
-sudo usermod -aG docker jenkins 
+if [ ! -f "/usr/bin/docker" ]; then
+    curl https://get.docker.com | sudo bash
+    sudo usermod -aG docker jenkins 
+fi
 
 # install docker compose
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
 echo "Test Stage"
+
+# venv created, source
+pytho3 -m venv venv
+source venv/bin/activate
+
+# pip3 installs, pytest, flask_testing, frontend requirements.txt and backend requirements.txt
+pip3 install pytest flask_testing
+pip3 install -r frontend/requirements.txt
+pip3 install -r backend/requirements.txt
+
+mkdir test_reports
+
+# run pytest frontend
+python3 -m pytest frontend \
+    --cov=frontend/application \
+    --cov-report term-misssing \
+    --cov-report xml:test_reports/frontend/coverage.xml \
+    --junitxml=test_reports/frontend/junit_report.xml
+
+# run pytes backend
+python3 -m pytest backend \
+    --cov=backend/application \
+    --cov-report term-misssing \
+    --cov-report xml:test_reports/backend/coverage.xml \
+    --junitxml=test_reports/backend/junit_report.xml
+
+
+# remove venv
+deactivate
+rm -rf venv
