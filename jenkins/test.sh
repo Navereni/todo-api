01ffeb74ef1ed680d68aeba1474ebbc845d6ed0a
Ubuntu <krzysztof@dev-jenkins.rtudhuqav13e5lf1sxn3p0wzyb.fx.internal.cloudapp.net>
2021-11-29 12:28:48 +0000

    fixing test.sh v2

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -18,14 +18,14 @@ python3 -m pytest frontend \
     --cov=frontend/application \
     --cov-report term-misssing \
     --cov-report xml:test_reports/frontend/coverage.xml \
-    --junitxml=test_reports/frontend/junit_report.xml
+    --junitxml=test_reports/frontend_junit_report.xml
 
 # run pytes backend
 python3 -m pytest backend \
     --cov=backend/application \
     --cov-report term-misssing \
     --cov-report xml:test_reports/backend/coverage.xml \
-    --junitxml=test_reports/backend/junit_report.xml
+    --junitxml=test_reports/backend_junit_report.xml
 
 
 # remove venv
